19ef94286dd0587b9b9fe9b36f380226366664f8
savioabuga <savioabuga@gmail.com>
2016-03-30 15:04:21 +0300

    started reactjs

--- components/App.js
+++ components/App.js
@@ -1,1 +1,12 @@
+import React, { Component } from 'react'
 
+class App extends Component {
+
+    render() {
+        return (
+            <div>This is a react app now!</div>
+            )
+    }
+}
+
+export default App
