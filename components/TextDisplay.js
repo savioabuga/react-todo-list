5e77c06638a4e4eda22c85de2eb27dfca942b3fa
savioabuga <savioabuga@gmail.com>
2016-04-01 22:29:07 +0300

    adding text display component

---
+++ components/TextDisplay.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class TextDisplay extends Component {
+
+    render() {
+        return <div>I'm display text {this.props.text}</div>
+    }
+
+
+}
+
+export default TextDisplay
--- components/TextInput.js
+++ components/TextInput.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
+import TextDisplay from './TextDisplay'
 
 class TextInput extends Component {
 
+    constructor(props, context) {
+        super(props, context)
+        this.state = {
+            inputText: 'initial text'
+        }
+    }
+
+    handleChange(event) {
+        this.setState({
+            inputText: event.target.value
+        })
+    }
+
     render() {
         return (
-                <div>This is another line</div>
+                <div>
+                    <input 
+                        type="text"
+                        placeholdder = "This is going to be text"
+                        value = {this.state.inputText}
+                        onChange = {this.handleChange.bind(this)}
+                    />
+                    <TextDisplay text={this.state.inputText}/>
+                </div>
             )
     }
 
